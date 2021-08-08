92f6dc4f781025b46c32565c835969be9ccad687
Nikita Ponomarenko <superprojects@live.ru>
2021-08-08 15:11:44 +0300

    Добавьте файлы проекта.

---
+++ AppHosting.Abstractions/Internal/IAppStartup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace AppHosting.Abstractions.Internal
+{
+    public interface IAppStartup
+    {
+        IServiceProvider ConfigureServices(IServiceCollection services);
+
+        void RegisterRoutes();
+    }
+}
